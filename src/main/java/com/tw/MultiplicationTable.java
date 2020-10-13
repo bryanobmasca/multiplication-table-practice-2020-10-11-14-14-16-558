cd793c47bd92929d1668cde928c12c67717dd373
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-13 22:34:22 +0800

    [MultiplicationTable][Bryan] feature: Add context map, and add the necessary function according to context map.

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,45 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        boolean isValid = isValid(startNumber, endNumber);
+        String multiplicationTable = createMultiplicationTable(startNumber, endNumber);
+        return isValid ? multiplicationTable : null;
+    }
+
+    private String createMultiplicationTable(int startNumber, int endNumber) {
+        String[] multiplcationLines = new String[(endNumber-startNumber) + 1];
+        for (int i = 0; i < multiplcationLines.length; i++){
+            multiplcationLines[i] = generateMultiplicationLine(startNumber, startNumber + i);
+        }
+        return String.join("\r\n", multiplcationLines);
+    }
+
+    private String generateMultiplicationLine(int num1, int num2) {
+        String[] multiplicationExpressions = new String[(num2-num1) + 1];
+        for (int i = 0; i < multiplicationExpressions.length; i++){
+            multiplicationExpressions[i] = generateMultiplicationExpression(num1 + i, num2);
+        }
+        return String.join("  ", multiplicationExpressions);
+    }
+
+    private String generateMultiplicationExpression(int multiplicand, int multiplier) {
+        return multiplicand + "*" + multiplier + "=" + (multiplicand*multiplier);
+    }
+
+
+    private boolean isValid(int startNum, int endNum) {
+        boolean isSmallerOrEqual = isSmallerOrEqual(startNum, endNum);
+        boolean isStartNumberInRange = isWithinRange(startNum);
+        boolean isEndNumberInRange = isWithinRange(endNum);
+
+        return isSmallerOrEqual && isStartNumberInRange && isEndNumberInRange;
+    }
+
+    private boolean isWithinRange(int number) {
+        return (number >= 1 && number <= 1000);
+    }
+
+    private boolean isSmallerOrEqual(int startNum, int endNum) {
+        return startNum <= endNum;
     }
 }
